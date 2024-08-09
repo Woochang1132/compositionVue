2fcb533d39d1224b97c92a74d1165f13ea6eecfc
Choi Woochang <show4031132@gmail.com>
2024-08-09 20:13:29 +0900

    [개발] grid component 적용하기

---
+++ src/components/AppGrid.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="row g-3">
+    <div v-for="(item, index) in items" :key="index" class="col-4"></div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  items: {
+    type: Array,
+    required: true
+  }
+})
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,17 +4,26 @@
     <p>{{ $route }}</p>
     <p>{{ $route.name }}</p>
     <button class="btn btn-primary" @click="goAboutPage">about 으로 이동</button>
+    <hr class="my-4" />
+    <div class="row g-3">
+      <div v-for="(item, index) in items" :key="index" class="col-4">
+        <AppCard>{{ item }}</AppCard>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { useRoute, useRouter } from 'vue-router'
+import { ref } from 'vue'
+import AppCard from '@/components/AppCard.vue'
 
 const route = useRoute()
 const router = useRouter()
 const goAboutPage = () => {
   router.push('/about')
 }
+const items = ref(['사과', '딸기', '포도', '바나나'])
 console.log('route.path >> ', route.path)
 </script>
 
